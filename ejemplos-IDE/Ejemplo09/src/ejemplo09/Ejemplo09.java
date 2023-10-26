71939bb0b48ba862165f4e64ffcea3c55468c4fb
valsam <mvsamaniego1@utpl.edu.ec>
2023-10-26 16:36:46 -0500

    Verificar cambios

---
+++ ejemplos-IDE/Ejemplo09/src/ejemplo09/Ejemplo09.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo09;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo dos");
+    }
+    
+}
---
+++ ejemplos-IDE/Ejemplo09/src/ejemplo09/Ejemplo12.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo09;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo12 
+{ public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo tres");
+    }
+    
+}
